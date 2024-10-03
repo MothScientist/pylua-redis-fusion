7406daa1557e41c9926abc5c4841ff1fc7f2897d
Moth <moth.feedback@gmail.com>
2024-10-03 17:10:48 +0300

    upd

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 from os import getenv
 from dotenv import load_dotenv
-from redis import Redis
+from redis import Redis, ConnectionPool
 
 class PyRedis:
-    def __init__(self, psw, db=0, port=6379, host='localhost'):
-        self.psw = psw
-        self.db = db  # database number
-        self.port = port
-        self.host = host
-        self.redis = self._connect()
+    def __init__(self, host, port, psw, db=0):
+        self.redis = Redis(connection_pool=ConnectionPool(host=host, port=port, db=db, password=psw))
 
-    def _connect(self) -> Redis:
-        return Redis(host=self.host, port=self.port, db=self.db, password=self.psw)
+    def set(
+            self,
+            key: str,
+            value,
+            time_ms=None,
+            time_s=None,
+            if_exist: bool = None,
+            if_not_exist: bool = None
+    ) -> None:
+        """
+        Set a new key or override an existing one
+        If both parameters (time_s, time_ms) are specified, the key will be deleted based on the smallest value.
+        :param key:
+        :param value:
+        :param time_ms:
+        :param time_s:
+        :param if_exist:
+        :param if_not_exist:
+        :return: None
+        """
+        self.redis.set(key, value, nx=if_not_exist, xx=if_exist, ex=time_s, px=time_ms)
 
-    def set(self, key, value) -> None:
-        self.redis.set(key, value)
+    def get(self, key, default_value=None):
+        res = self.redis.get(key)
+        return res if res else default_value
 
-    def get(self, key):
-        return self.redis.get(key)
-
-    def delete(self, key) -> None:
+    def delete(self, key, returning: bool = False):
+        """
+        Delete a key
+        :param key:
+        :param returning: Should I return the value that the key had before deletion?
+        :return: value or None
+        """
+        res = self.redis.get(key) if returning else None
         self.redis.delete(key)
+        return res
 
 
 load_dotenv('redis.env')  # Load environment variables from redis.env file
@@ -29,9 +50,4 @@ redis_db: int = int(getenv('REDIS_DB'))
 redis_host: str = getenv('REDIS_HOST')
 redis_port: int = int(getenv('REDIS_PORT'))
 
-r = Redis(host=redis_host, port=redis_port, db=redis_db, password=redis_psw)
-
-r.set('first', 1)
-print(r.get('first'))
-r.delete('first')
-print(r.get('first'))
+r = PyRedis(redis_host, redis_port, redis_psw, db=redis_db)
