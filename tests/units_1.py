91e43be637717b5b7f0e0205ced7d809ddc10cc0
Moth <moth.feedback@gmail.com>
2025-01-12 17:30:17 +0300

    [tests] fix import

--- tests/units_1.py
+++ tests/units_1.py
@@ -1,12 +1,13 @@
 import unittest
 
-from src.client import PyRedis, redis_psw, redis_db, redis_host, redis_port
+from ..src.client import PyRedis, redis_psw, redis_db, redis_host, redis_port
 
 
 class SmokeTests(unittest.TestCase):
-	def test_ping_001(self):
-		r = PyRedis(redis_host, redis_port, redis_psw, db=redis_db, socket_timeout=.001)
-		self.assertTrue(r.r_ping())
+	r = PyRedis(redis_host, redis_port, redis_psw, db=redis_db, socket_timeout=.001)
+
+	def test_ping(self):
+		self.assertTrue(SmokeTests.r.r_ping())
 
 
 if __name__ == '__main__':
