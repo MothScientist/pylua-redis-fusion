2e0357df7a9731e7cd0435a2eef4af8b89f125bc
Moth <moth.feedback@gmail.com>
2025-02-06 22:36:41 +0300

    [CI/CD] updating and adding .yml files

--- pyredis_install.sh
+++ pyredis_install.sh
@@ -8,30 +8,27 @@ COLOR_CYAN="\033[36m"
 echo "${COLOR_CYAN}SwiftPipRedis${COLOR_RESET}"
 
 echo "${COLOR_GREEN}Git clone${COLOR_RESET}"
-git clone https://github.com/MothScientist/SwiftPipRedis.git
-
-# shellcheck disable=SC2181
-if [ $? -ne 0 ]; then
-    echo "${COLOR_RED}Error installing Lua 5.4${COLOR_RESET}"
+if ! git clone https://github.com/MothScientist/SwiftPipRedis.git; then
+    echo "${COLOR_RED}Error installing SwiftPipRedis${COLOR_RESET}"
     exit 1
 fi
 
 if [ "$1" = "--requirements-install" ]; then
     echo "${COLOR_GREEN}Upgrade pip${COLOR_RESET}"
-    pip install --upgrade pip
-
-    # shellcheck disable=SC2181
-    if [ $? -ne 0 ]; then
+    if ! pip install --upgrade pip; then
         echo "${COLOR_RED}Error upgrading pip${COLOR_RESET}"
         exit 1
     fi
 
     echo "${COLOR_GREEN}Install requirements.txt${COLOR_RESET}"
-    pip install -r requirements.txt
-
-    # shellcheck disable=SC2181
-    if [ $? -ne 0 ]; then
+    if ! pip install -r requirements.txt; then
         echo "${COLOR_RED}Error installing requirements.txt${COLOR_RESET}"
+        exit 1
+    fi
+
+    echo "${COLOR_GREEN}Updating requirements.txt${COLOR_RESET}"
+    if ! pip freeze > requirements.txt; then
+        echo "${COLOR_RED}Error updating requirements.txt${COLOR_RESET}"
         exit 1
     fi
 fi
