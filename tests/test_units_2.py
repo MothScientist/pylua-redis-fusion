4f8e24e763eff4d990f6a22628780578bda3bbaf
Moth <moth.feedback@gmail.com>
2025-02-09 15:06:50 +0300

    [tests] update TtlTests

--- tests/test_units_2.py
+++ tests/test_units_2.py
@@ -4,6 +4,7 @@ from os import getenv
 from random import randint, choice, random
 from string import ascii_letters, digits
 from sys import path as sys_path
+from time import sleep
 
 sys_path.append('../')
 from src.client import PyRedis
@@ -18,8 +19,8 @@ redis_username: str = getenv('REDIS_USERNAME') or 'default'
 
 class TtlTests(unittest.TestCase):
 	"""
-	Tests to check the lifetime of keys, the sleep() function will be used,
-	so all other parameters are optimized as much as possible
+	Tests to check the service life of keys, the sleep() function will be used,
+	so the tests are run in parallel on all processor cores
 	"""
 	# def setUp(self):
 	# 	self.maxDiff = None
@@ -33,6 +34,9 @@ class TtlTests(unittest.TestCase):
 		socket_timeout=.1
 	)
 
+	def setUp(self):
+		TtlTests.r.r_remove_all_keys()
+
 	@staticmethod
 	def get_random_integer():
 		return randint(0, 100)
@@ -44,6 +48,102 @@ class TtlTests(unittest.TestCase):
 	def test_ping(self):
 		self.assertTrue(TtlTests.r.r_ping())
 
+	def test_set_get_ttl_str_001(self):
+		key: str = 'set_get_str_001'
+		value: str = TtlTests.get_random_string()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=1))
+		res_1: str = TtlTests.r.r_get(key)
+		self.assertEqual(res_1, value)
+		sleep(1)
+		res_2: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_2, f'res = {res_2}')
+
+	def test_set_get_ttl_str_002(self):
+		key: str = 'set_get_str_002'
+		value: str = TtlTests.get_random_string()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=5))
+		res_1: str = TtlTests.r.r_get(key)
+		self.assertEqual(res_1, value)
+		sleep(5)
+		res_2: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_2, f'res = {res_2}')
+
+	def test_set_get_ttl_str_003(self):
+		key: str = 'set_get_str_003'
+		value: str = TtlTests.get_random_string()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=1, time_ms=5000))
+		res_1: str = TtlTests.r.r_get(key)
+		self.assertEqual(res_1, value)
+		sleep(1)
+		res_2: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_2, f'res = {res_2}')
+
+	def test_set_get_ttl_str_004(self):
+		key: str = 'set_get_str_004'
+		value: str = TtlTests.get_random_string()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=10, time_ms=5000))
+		res_1: str = TtlTests.r.r_get(key)
+		self.assertEqual(res_1, value)
+		sleep(5)
+		res_2: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_2, f'res = {res_2}')
+
+	def test_set_get_ttl_str_005(self):
+		key: str = 'set_get_str_005'
+		value: str = TtlTests.get_random_string()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=0.1, time_ms=100))
+		res_1 = TtlTests.r.r_get(key)
+		self.assertEqual(res_1, value)
+		sleep(1)
+		res_2: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_2, f'res = {res_2}')
+
+	def test_set_get_ttl_int_001(self):
+		key: str = 'set_get_int_001'
+		value: int = TtlTests.get_random_integer()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=1))
+		res_1: str = TtlTests.r.r_get(key)
+		self.assertEqual(res_1, str(value))
+		sleep(1)
+		res_2: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_2, f'res = {res_2}')
+
+	def test_set_get_ttl_int_002(self):
+		key: str = 'set_get_int_002'
+		value: int = TtlTests.get_random_integer()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=3))
+		res_1: str = TtlTests.r.r_get(key, convert_to_type='int')
+		self.assertEqual(res_1, value)
+		sleep(1)
+		res_2: str = TtlTests.r.r_get(key, convert_to_type='integer')
+		self.assertEqual(res_2, value)
+		sleep(2)
+		res_3: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_3, f'res = {res_2}')
+
+	def test_set_get_ttl_float_001(self):
+		key: str = 'set_get_float_001'
+		value: float = random()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=2))
+		res_1: str = TtlTests.r.r_get(key)
+		self.assertEqual(res_1, str(value))
+		sleep(2)
+		res_2: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_2, f'res = {res_2}')
+
+	def test_set_get_ttl_float_002(self):
+		key: str = 'set_get_float_002'
+		value: float = random()
+		self.assertIsNone(TtlTests.r.r_set(key, value, time_s=3))
+		res_1: str = TtlTests.r.r_get(key, convert_to_type='numeric')
+		self.assertEqual(res_1, value)
+		sleep(1)
+		res_2: str = TtlTests.r.r_get(key, convert_to_type='double')
+		self.assertEqual(res_2, value)
+		sleep(2)
+		res_3: None = TtlTests.r.r_get(key)
+		self.assertIsNone(res_3, f'res = {res_2}')
+
 
 if __name__ == '__main__':
 	from redis import Redis, ConnectionPool
@@ -51,5 +151,3 @@ if __name__ == '__main__':
 		host=redis_host,port=redis_port,db=0,password=redis_psw, username=redis_username
 	))
 	_redis.flushall()  # clear the database before tests
-
-	unittest.main()
