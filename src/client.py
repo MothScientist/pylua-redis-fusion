54e65761412820eebb785a8fe8981826881e083e
Moth <moth.feedback@gmail.com>
2025-02-17 15:58:38 +0300

    [redis] adding new functions: key_is_exist, keys_is_exist, get_count_of_keys, set_key_ttl, set_keys_ttl, get_key_ttl, drop_key_ttl, drop_keys_ttl, get_type_value_of_key, r_mass_unlink Function removed: r_mass_check_keys_exists

--- src/client.py
+++ src/client.py
@@ -42,7 +42,9 @@ class PyRedis:
             'get_helper': PyRedis.__load_lua_script('get_helper'),
             'delete_with_returning': PyRedis.__load_lua_script('delete_with_returning'),
             'unlink_with_returning': PyRedis.__load_lua_script('unlink_with_returning'),
-            'set_not_array_helper': PyRedis.__load_lua_script('set_not_array_helper')
+            'set_not_array_helper': PyRedis.__load_lua_script('set_not_array_helper'),
+            'set_keys_ttl': PyRedis.__load_lua_script('set_keys_ttl'),
+            'drop_keys_ttl': PyRedis.__load_lua_script('drop_keys_ttl'),
         }
 
     def r_ping(self) -> bool:
@@ -51,8 +53,69 @@ class PyRedis:
         except (rConnectionError, rTimeoutError):
             return False
 
-    def key_is_exist(self, key: str) -> bool:
-        return bool(self.redis.exists(key))
+    def key_is_exist(self, key: str) -> bool | None:
+        return bool(self.redis.exists(key)) if key else None
+
+    def keys_is_exist(self, keys: list[str] | tuple[str] | set[str] | frozenset[str]) -> int:
+        """
+        Mass operation to check for keys
+        """
+        return self.redis.exists(*keys) if keys else 0
+
+    def get_count_of_keys(self) -> int:
+        """ Returns the number of keys in the current database """
+        return self.redis.dbsize()
+
+    def set_key_ttl(
+            self,
+            key: str,
+            ttl_sec: int | None = None,
+            ttl_ms: int | None = None
+    ) -> None:
+        """ Set key time (ttl) in seconds or milliseconds """
+        if ttl_ms := PyRedis.__compare_and_select_sec_ms(ttl_sec, ttl_ms) if (ttl_sec or ttl_ms) else None:
+            self.redis.pexpire(key, ttl_ms)
+
+    def set_keys_ttl(
+            self,
+            keys: list[str] | tuple[str] | set[str] | frozenset[str],
+            ttl_sec: int | None = None,
+            ttl_ms: int | None = None
+    ) -> None:
+        keys: tuple = PyRedis.__remove_duplicates(keys)
+        ttl_ms = PyRedis.__compare_and_select_sec_ms(ttl_sec, ttl_ms) if ((ttl_sec or ttl_ms) and keys) else None
+        if ttl_ms:
+            self.__register_lua_scripts('set_keys_ttl', len(keys), *keys, ttl_ms)
+
+    def get_key_ttl(self, key: str, in_seconds: bool = False) -> int | None:
+        """
+        Returns the remaining time to live of a key that has a timeout
+        :param key:
+        :param in_seconds:
+        :return: ttl in seconds;
+        return None if the key does not exist;
+        return 0 if the key exists but has no associated expire;
+        """
+        res = self.redis.ttl(key) if in_seconds else self.redis.pttl(key)
+        return res if res not in (-1, -2) else (0 if res == -1 else None)
+
+    def drop_key_ttl(self, key: str):
+        """ Removes the key lifetime (ttl) if one is set """
+        if key:
+            self.redis.persist(key)
+
+    def drop_keys_ttl(self, keys: list[str] | tuple[str] | set[str] | frozenset[str]):
+        if keys := PyRedis.__remove_duplicates(keys):
+            self.__register_lua_scripts('drop_keys_ttl', len(keys), *keys)
+
+    def get_type_value_of_key(self, key: str) -> str | None:
+        """
+        The type of value stored at this key
+        :param key:
+        :return: str or None
+        """
+        res = self.redis.type(key) if key else None
+        return res if res != 'none' else None
 
     def r_set(
             self,
@@ -217,18 +280,35 @@ class PyRedis:
             exists_key_value if get_dict_key_value_exists else {}
         )
 
-    def r_mass_check_keys_exists(self, keys: list | tuple | set | frozenset) -> tuple:
-        """
-        Mass operation to check for keys
-        """
+    def r_mass_unlink(
+            self,
+            keys: list | tuple | set | frozenset,
+            return_exists: bool = False,
+            return_non_exists: bool = False,
+            get_dict_key_value_exists: bool = False,
+            convert_to_type_dict_key: str = None
+    ) -> tuple[tuple, tuple, dict]:  # TODO - Lua
         if not keys:
-            return ()
+            return (), (), {}
 
         keys: tuple = PyRedis.__remove_duplicates(keys)  # remove duplicates
-        existing_keys = self.redis.mget(keys)
 
-        # filter only those keys whose values is not None
-        return tuple(keys[i] for i, value in enumerate(existing_keys) if value is not None)
+        # all parameters = None (None is None is None -> True)
+        if return_exists is return_non_exists is get_dict_key_value_exists is False:
+            self.redis.unlink(*keys)
+            return (), (), {}
+
+        # if one of the parameters is specified, then we collect a dictionary of existing key-values
+        exists_key_value: dict = self.check_keys_and_get_values(keys, convert_to_type_dict_key=convert_to_type_dict_key)
+        exists_keys: tuple = tuple(exists_key_value.keys())
+        non_exists_keys: tuple = tuple(frozenset(keys).difference(frozenset(exists_keys)))
+        self.redis.unlink(*exists_keys)
+
+        return (
+            exists_keys if return_exists else (),
+            non_exists_keys if return_non_exists else (),
+            exists_key_value if get_dict_key_value_exists else {}
+        )
 
     def check_keys_and_get_values(
             self, keys: list | tuple | set | frozenset,
