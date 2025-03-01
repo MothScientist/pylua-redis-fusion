c2c5465c5efc144fc35739cf501264aabf35c63e
Moth <moth.feedback@gmail.com>
2025-03-01 13:17:56 +0300

    [tests] hotfix

--- tests/connection_params.py
+++ tests/connection_params.py
@@ -4,5 +4,5 @@ from os import getenv
 load_dotenv('../src/redis.env')  # Load environment variables from redis.env file
 REDIS_PWS: str = getenv('REDIS_PSW')
 REDIS_HOST: str = getenv('REDIS_HOST') or 'localhost'
-REDIS_PORT: int = int(getenv('REDIS_PORT')) or 6379
+REDIS_PORT: int = int(getenv('REDIS_PORT') or 6379)
 REDIS_USERNAME: str = getenv('REDIS_USERNAME') or 'default'
