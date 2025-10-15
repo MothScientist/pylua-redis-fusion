3b66ef42774da1c8fdc01caea87b4811e3ab7c48
Moth <moth.feedback@gmail.com>
2025-10-15 20:47:01 +0300

    [__init__] added __version__

--- pyluaredis/__init__.py
+++ pyluaredis/__init__.py
@@ -1,2 +1,9 @@
-from .client import PyRedis
-from .data_type_converter import TypeConverter
+from importlib.metadata import version, PackageNotFoundError
+from pyluaredis.client import PyRedis
+from pyluaredis.data_type_converter import TypeConverter
+
+
+try:
+    __version__ = version('pyluaredis')
+except PackageNotFoundError:
+    __version__ = 'No package metadata was found for pyluaredis'
