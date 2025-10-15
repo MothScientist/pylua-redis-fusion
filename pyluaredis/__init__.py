f5eb7da18387d5e897168b2ee4cb10d4ccbbfef4
Moth <moth.feedback@gmail.com>
2025-10-15 21:26:35 +0300

    [__init__] fix #1

--- pyluaredis/__init__.py
+++ pyluaredis/__init__.py
@@ -2,14 +2,17 @@ from importlib.metadata import metadata, version, PackageNotFoundError
 from pyluaredis.client import PyRedis
 from pyluaredis.data_type_converter import TypeConverter
 
-package_metadata = metadata('pyluaredis')
-
-__author__ = package_metadata.get('Author', '')
-__license__ = package_metadata.get('License', '')
-__description__ = package_metadata.get('Summary', '')
-__url__ = package_metadata.get('Home-page', '')
 
 try:
+    package_metadata = metadata('pyluaredis')
+
+    __author__ = package_metadata.get('Author', '')
+    __license__ = package_metadata.get('License', '')
+    __description__ = package_metadata.get('Summary', '')
+    __url__ = package_metadata.get('Home-page', '')
     __version__ = version('pyluaredis')
 except PackageNotFoundError:
     __version__ = 'No package metadata was found for pyluaredis'
+    __license__ = 'No package metadata was found for pyluaredis'
+    __description__ = 'No package metadata was found for pyluaredis'
+    __url__ = 'No package metadata was found for pyluaredis'
