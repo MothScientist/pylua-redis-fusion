3847131d4635726b167bc51de506fa3415f44195
Moth <moth.feedback@gmail.com>
2025-10-15 21:18:55 +0300

    [__init__, pyproject.toml] upd

--- pyluaredis/__init__.py
+++ pyluaredis/__init__.py
@@ -1,7 +1,13 @@
-from importlib.metadata import version, PackageNotFoundError
+from importlib.metadata import metadata, version, PackageNotFoundError
 from pyluaredis.client import PyRedis
 from pyluaredis.data_type_converter import TypeConverter
 
+package_metadata = metadata('pyluaredis')
+
+__author__ = package_metadata.get('Author', '')
+__license__ = package_metadata.get('License', '')
+__description__ = package_metadata.get('Summary', '')
+__url__ = package_metadata.get('Home-page', '')
 
 try:
     __version__ = version('pyluaredis')
